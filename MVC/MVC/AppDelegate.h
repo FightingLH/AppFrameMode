415489ca469a1c4a1f4277f52b3eaef0c5c5041f
lihuan <lihuan@feeyo.com>
2018-10-15 14:13:11 +0800

    测试msg

---
+++ MVC/MVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVC
+//
+//  Created by feeyo on 2018/10/15.
+//  Copyright © 2018年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVC/MVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVC
+//
+//  Created by feeyo on 2018/10/15.
+//  Copyright © 2018年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
